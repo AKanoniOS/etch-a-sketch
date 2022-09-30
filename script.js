1a8bed188c805e090349365b06d4168c662dd2d9
Steve Schmeltzer <schmeltzersteve@gmailcom>
2022-09-30 14:59:02 -0800

    add low, med, high res options

--- script.js
+++ script.js
@@ -2,35 +2,55 @@ let container = document.createElement('div');
 container.classList.add('container');
 document.body.appendChild(container);
 
-buttons = document.querySelectorAll('button');
-
-createCells('low-res');
-
-buttons.forEach(button => {
-    button.addEventListener('click', createCells(`${button.className}`));
-});
-
 function createCells(res) {
     if (res == 'low-res') {
-        for (i=0;i<63;i++){
+        for (i=0;i<378;i++){
             let newDiv = document.createElement('div');
             newDiv.id = 'cell'+i;
-            newDiv.className = 'cell';
+            newDiv.classList.add("cell");
+            newDiv.classList.add("low-res");
             container.appendChild(newDiv);
             newDiv.addEventListener('mouseover', function (e) {
                 e.target.style.background = 'blue';
               });
         }
     } else if (res == 'med-res') {
-        for (i=0;i<250;i++){
+        for (i=0;i<1566;i++){
             let newDiv = document.createElement('div');
             newDiv.id = 'cell'+i;
-            newDiv.className = 'cell';
+            newDiv.classList.add("cell");
+            newDiv.classList.add("med-res");
+            container.appendChild(newDiv);
+            newDiv.addEventListener('mouseover', function (e) {
+                e.target.style.background = 'blue';
+              });
+        }
+    } else if (res == 'high-res') {
+        for (i=0;i<8643;i++){
+            let newDiv = document.createElement('div');
+            newDiv.id = 'cell'+i;
+            newDiv.classList.add("cell");
+            newDiv.classList.add("high-res");
             container.appendChild(newDiv);
             newDiv.addEventListener('mouseover', function (e) {
                 e.target.style.background = 'blue';
               });
         }
     }
-    
+}
+
+// add reset function
+function reset() {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(item => {
+        item.style.background = 'red';
+    })
+}
+
+// add delete function
+function deleteCells() {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(item => {
+        container.removeChild(item);
+    })
 }
